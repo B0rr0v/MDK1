5197fe13c328daaddbc990d1748edd8b7e765368
ИСП-31 <-31@LAB17-05>
2023-11-03 13:04:51 +0300

    Добавьте файлы проекта.

---
+++ Lib_5/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_5
+{
+    public class Class1
+    {
+        public static void разработка(int k, out String outputtb1, out int count1, out int sum1)
+        {
+            int count = 0;//счетчик числ
+            String outputtb = "";// числа в tb
+            int number = 0;
+            Random rnd = new Random();
+            int sum = 0;
+            while (sum < k)
+            {
+                number = rnd.Next(-4, 7);
+                sum += number;
+               
+                outputtb = String.Concat(outputtb, Convert.ToString(number));//объединяем строки в одну
+                outputtb = String.Concat(outputtb, ",");// запятая между числами
+                count++;
+            }
+            outputtb = outputtb.Substring(0, outputtb.Length - 1);//удаляем посл элемент(запятую)
+            outputtb1 = outputtb;
+            count1 = count;
+            sum1 = sum - number;
+        }
+    }
+}
---
+++ MDK1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_5;
+//Вычислить сумму целых случайных чисел, распределенных в диапазоне от - 7 до 3,
+//пока эта сумма не превышает некоторого числа K. Вывести на экран
+//сгенерированные числа, значение суммы, и количество сгенерированных чисел.
+
+namespace MDK1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void About(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Разработчик: Агальцов Даниил ИСП-31 Практическая работа №1:Вычислить сумму целых случайных чисел, распределенных в диапазоне от - 7 до 3, пока эта сумма не превышает некоторого числа K.Вывести на экран сгенерированные числа, значение суммы, и количество сгенерированных чисел.");
+        }
+
+        private void Генерация(object sender, RoutedEventArgs e)
+        {
+            bool f = Int32.TryParse(chislK.Text, out int y);// проверка на числовой тип данных
+            if (f)
+            {
+                Lib_5.Class1.разработка(y,out String outputtb1, out int count1, out int sum1);
+
+                tb1.Text = outputtb1;
+                kolvochisl.Text = Convert.ToString(count1);
+                cymma.Text = Convert.ToString(sum1);
+            } else { MessageBox.Show("ошибка");  }
+        }
+
+        private void Очистить(object sender, RoutedEventArgs e)// очистка полей в tb
+        {
+            cymma.Clear();
+            kolvochisl.Clear();
+            tb1.Clear();
+            chislK.Clear();
+        }
+    }
+}
